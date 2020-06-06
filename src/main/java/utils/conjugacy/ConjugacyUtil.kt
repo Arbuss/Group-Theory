d86a1fe924d80f43e2c0e54ac4a9da359ddc1b1f
Artur Buss <artur.buss@bytepace.com>
2020-06-06 18:14:21 +0600

    calculating conjugacy classes fine

--- src/main/java/utils/conjugacy/ConjugacyUtil.kt
+++ src/main/java/utils/conjugacy/ConjugacyUtil.kt
@@ -1,31 +1,59 @@
 package utils.conjugacy
 
+import field.IIntegerFromMultiplicativeField
 import matrix.IMatrix
 import utils.matrix.MatrixUtil
 
-typealias ConjugacyClass<T> = List<IMatrix<T>>
+typealias ConjugacyClass<T> = ArrayList<IMatrix<T>>
 
 class ConjugacyUtil {
     companion object {
-        fun <T> getConjugacyClasses(group: List<IMatrix<T>>) {
+        fun <T> getConjugacyClasses(group: List<IMatrix<T>>): ArrayList<ConjugacyClass<T>> {
+            val tempGroup = group.toMutableList()
             val conjugacyClasses = ArrayList<ConjugacyClass<T>>()
-            val rawTriplets = ArrayList<Triple<IMatrix<T>, IMatrix<T>, IMatrix<T>>>()
 
-            for(g1 in group) {
-                for(g2 in group) {
-                    if(g1 == g2) continue
-                    for(h in group) {
-                        val someMatrix = h * g1 * h.getInvertible()
-                        if(g2 == someMatrix) {
-                            rawTriplets.add(Triple(g1, g2, h))
-                        }
-                    }
+            //a~b : hah^-1 = b
+            var classesCount = 0
+            while (tempGroup.isNotEmpty()) {
+                conjugacyClasses.add(arrayListOf())
+                val conjugacyClass = getClass(group, tempGroup.first())
+                conjugacyClasses[classesCount].addAll(conjugacyClass)
+                tempGroup.removeAll(conjugacyClass)
+                classesCount++
+            }
+            printClasses(conjugacyClasses)
+            return conjugacyClasses
+        }
+
+        fun<T> printClasses(classes: ArrayList<ConjugacyClass<T>>) {
+            for(conjugacyClass in classes) {
+                printClass(conjugacyClass)
+            }
+        }
+
+        private fun<T> printClass(conjugacyClass: ConjugacyClass<T>) {
+            val lineLength = conjugacyClass.size * 11 - 1
+            printLine(lineLength)
+            MatrixUtil.printMatrix(conjugacyClass)
+            printLine(lineLength)
+        }
+
+        private fun printLine(length: Int) {
+            for(i in 0 until length) {
+                print("-")
+            }
+            println()
+        }
+
+        private fun<T> getClass(group: List<IMatrix<T>>, lvalue: IMatrix<T>): ConjugacyClass<T> {
+            val conjugacyClass = ConjugacyClass<T>()
+            for(h in group) {
+                val supposedB = h * lvalue * h.getInvertible()
+                if(!conjugacyClass.contains(supposedB)) {
+                    conjugacyClass.add(supposedB)
                 }
             }
-            println(rawTriplets.size)
-            rawTriplets.forEach {
-                MatrixUtil.printMatrix(arrayListOf(it.first, it.second, it.third))
-            }
+            return conjugacyClass
         }
     }
 }
